7e2a4a20c3672d7e274a8d4e7ca89ea8bd113899
Esraa-Khairy <Esraa.akhairy@gmail.com>
2020-04-02 09:00:44 +0200

    NVIC_updated

--- 01-MCAL/04-NVIC_DRIVER/nvic.c
+++ 01-MCAL/04-NVIC_DRIVER/nvic.c
@@ -17,7 +17,9 @@
 #define 	NVIC_NonGroupPriority		0x00000700
 /********************************************************************/
 
-#define NVIC_AIRC 		*((volatile u32*)0xE000ED0C)    // to set number of sub group bits
+#define   NVIC_AIRC 		  *((volatile u32*)0xE000ED0C)    // to set number of sub group bits
+#define 	MAX_NUM_IRQ 		240
+#define 	BITS_NUM			  32
 
 
 
@@ -25,54 +27,19 @@
 typedef struct
 {
 	u32 Padding_0[64];
-	u32 SETENA0;
-	u32 SETENA1;
-	u32 SETENA2;
-	u32 SETENA3;
-	u32 SETENA4;
-	u32 SETENA5;
-	u32 SETENA6;
-	u32 SETENA7;
-	u32 Dummy_Arr1 [24];
-	u32 CLRENA0;
-	u32 CLRENA1;
-	u32 CLRENA2;
-	u32 CLRENA3;
-	u32 CLRENA4;
-	u32 CLRENA5;
-	u32 CLRENA6;
-	u32 CLRENA7;
-	u32 Dummy_Arr2 [24];
-	u32 SETPEND0;
-	u32 SETPEND1;
-	u32 SETPEND2;
-	u32 SETPEND3;
-	u32 SETPEND4;
-	u32 SETPEND5;
-	u32 SETPEND6;
-	u32 SETPEND7;
+	u32 SETENA[8];
+	u32 Dummy_Arr1[24];
+	u32 CLRENA[8];
+	u32 Dummy_Arr2[24];
+	u32 SETPEND[8];
 	u32 Dummy_arr3[24];
-	u32 CLRPEND0;
-	u32 CLRPEND1;
-	u32 CLRPEND2;
-	u32 CLRPEND3;
-	u32 CLRPEND4;
-	u32 CLRPEND5;
-	u32 CLRPEND6;
-	u32 CLRPEND7;
+	u32 CLRPEND[8];
 	u32 Dummy_arr4[24];
-	u32 ACTIVE0;
-	u32 ACTIVE1;
-	u32 ACTIVE2;
-	u32 ACTIVE3;
-	u32 ACTIVE4;
-	u32 ACTIVE5;
-	u32 ACTIVE6;
-	u32 ACTIVE7;
-	u32 Dummy_arr5[56];  
-	u8  IPR[240];       
-	u32 Dummy_arr6[644]; 
-  u32 STIR;            
+	u32 ACTIVE[8];
+	u32 Dummy_arr5[56];
+	u8  IPR[240];
+	u32 Dummy_arr6[644];
+	u32 STIR;            
 
 }NVIC_t;
 
@@ -99,7 +66,7 @@ void NVIC_voidInitPriority (void)
  * Input:  Interrupt request number 0:240 , PriorityLevel
  * Output: void
  * */
-void NVIC_u8SetPriority(u32 Copy_u8IntReqNum , u8 Copy_u8PriorityLevel)
+void NVIC_u8SetPriority(u8 Copy_u8IntReqNum , u8 Copy_u8PriorityLevel)
 {
 
 		NVIC-> IPR [Copy_u8IntReqNum] = Copy_u8PriorityLevel;
@@ -113,25 +80,15 @@ void NVIC_u8SetPriority(u32 Copy_u8IntReqNum , u8 Copy_u8PriorityLevel)
  * Input: Interrupt request number NVIC_IRQx
  * output: Error_s  */
 
-ERROR_STATUS NVIC_u8EnableIRQ (u32 Copy_u8IntReqNum)
+ERROR_STATUS NVIC_u8EnableIRQ (u8 Copy_u8IntReqNum)
 {
 	u8 Local_u8Error = status_Ok;
-
-	if (Copy_u8IntReqNum < 32 )
+  
+  if (Copy_u8IntReqNum < MAX_NUM_IRQ)
 	{
-		NVIC -> SETENA0 |= Copy_u8IntReqNum;
+		NVIC -> SETENA [Copy_u8IntReqNum/BITS_NUM] |= (1<<(Copy_u8IntReqNum % BITS_NUM));
 	}
-	else if (Copy_u8IntReqNum < 64 )
-	{
-		NVIC -> SETENA1 |= Copy_u8IntReqNum;
-	}
-
-	else if (Copy_u8IntReqNum < 96 )
-	{
-
-		NVIC -> SETENA2 |= Copy_u8IntReqNum;
-	}
-
+\
 	else
 	{
 		Local_u8Error = status_NOk;
@@ -147,25 +104,14 @@ ERROR_STATUS NVIC_u8EnableIRQ (u32 Copy_u8IntReqNum)
  * Input: Interrupt request number NVIC_IRQx
  * output: Error_s  */
 
-ERROR_STATUS NVIC_u8DisbaleIRQ (u32 Copy_u8IntReqNum)
+ERROR_STATUS NVIC_u8DisbaleIRQ (u8 Copy_u8IntReqNum)
 {
 	u8 Local_u8Error = status_Ok;
 
-		if (Copy_u8IntReqNum < 32 )
+		if (Copy_u8IntReqNum < MAX_NUM_IRQ)
 		{
-			NVIC -> CLRENA0 |= Copy_u8IntReqNum;
+      NVIC -> CLRENA [Copy_u8IntReqNum/BITS_NUM] |= (1<<(Copy_u8IntReqNum % BITS_NUM));
 		}
-		else if (Copy_u8IntReqNum < 64 )
-		{
-			NVIC -> CLRENA1 |= Copy_u8IntReqNum;
-		}
-
-		else if (Copy_u8IntReqNum < 96 )
-		{
-
-			NVIC -> CLRENA2 |= Copy_u8IntReqNum;
-		}
-
 		else
 		{
 			Local_u8Error = status_NOk;
@@ -177,17 +123,13 @@ ERROR_STATUS NVIC_u8DisbaleIRQ (u32 Copy_u8IntReqNum)
 /*Description : This API shall get active status of interrupt
  * Input: Interrupt request number NVIC_IRQx
  * output: u8 active status zero if not active and larger than 1 if active */
-u8 NVIC_u8IsActive (u32 Copy_u8IntReqNum)
+u8 NVIC_u8IsActive (u8 Copy_u8IntReqNum)
 {
 	u8 u8IsActive_val = 0;
-	if (Copy_u8IntReqNum < 32)
-	{
-		u8IsActive_val = (NVIC -> ACTIVE0) & Copy_u8IntReqNum ;
-	}
-	else if (Copy_u8IntReqNum <64)
-	{
-		u8IsActive_val = (NVIC -> ACTIVE1) & Copy_u8IntReqNum ;
-	}
+	if (Copy_u8IntReqNum <MAX_NUM_IRQ)
+		{
+			u8IsActive_val = (NVIC -> ACTIVE[Copy_u8IntReqNum/BITS_NUM]) &  (1<< (Copy_u8IntReqNum % BITS_NUM));
+		}
 
 	return (u8IsActive_val);
 }
@@ -198,7 +140,7 @@ u8 NVIC_u8IsActive (u32 Copy_u8IntReqNum)
  * Input: Interrupt request number NVIC_IRQx
  * output: u8 priority value */
 
-u8 NVIC_u8GetPriority (u32 Copy_u8IntReqNum)
+u8 NVIC_u8GetPriority (u8 Copy_u8IntReqNum)
 {
 	u8 u8priority_val;
 
@@ -215,21 +157,13 @@ u8 NVIC_u8GetPriority (u32 Copy_u8IntReqNum)
  * Input: Interrupt request number NVIC_IRQx
  * output: u8 ERROR STATUS*/
 
-ERROR_STATUS NVIC_u8SWIntGenerate(u32 Copy_u8IntReqNum)  //NVIC_SetPendingIRQ(3);NVIC->STIR = 3;
+ERROR_STATUS NVIC_u8SWIntGenerate(u8 Copy_u8IntReqNum)  //NVIC_SetPendingIRQ(3);NVIC->STIR = 3;
 {
 	u8 Local_u8Error = status_Ok;
-	if (Copy_u8IntReqNum <32)
-	{
-		NVIC -> SETPEND0 |= Copy_u8IntReqNum;
-	}
-	else if (Copy_u8IntReqNum < 64)
-	{
-		NVIC -> SETPEND1|= Copy_u8IntReqNum;
-	}
-	else if (Copy_u8IntReqNum < 96)
-	{
-		NVIC -> SETPEND2 |= Copy_u8IntReqNum;
-	}
+	if (Copy_u8IntReqNum < MAX_NUM_IRQ)
+			{
+				NVIC -> SETPEND [Copy_u8IntReqNum/BITS_NUM] |= (1<<(Copy_u8IntReqNum % BITS_NUM));
+			}
 	else
 	{
 		Local_u8Error = status_NOk;
@@ -246,26 +180,18 @@ ERROR_STATUS NVIC_u8SWIntGenerate(u32 Copy_u8IntReqNum)  //NVIC_SetPendingIRQ(3)
  * Input: Interrupt request number NVIC_IRQx
  * output: u8 ERROR STATUS*/
 
-ERROR_STATUS NVIC_u8ClearPending (u32 Copy_u8IntReqNum)
+ERROR_STATUS NVIC_u8ClearPending (u8 Copy_u8IntReqNum)
 {
 	u8 Local_u8Error = status_Ok;
-		if (Copy_u8IntReqNum <32)
-		{
-			NVIC -> SETPEND0 &= ~Copy_u8IntReqNum;
-		}
-		else if (Copy_u8IntReqNum < 64)
-		{
-			NVIC -> SETPEND1 &= ~Copy_u8IntReqNum;
-		}
-		else if (Copy_u8IntReqNum < 96)
-		{
-			NVIC -> SETPEND2 &= ~Copy_u8IntReqNum;
-		}
-		else
-		{
-			Local_u8Error = status_NOk;
-		}
-		return (Local_u8Error);
+	if (Copy_u8IntReqNum < MAX_NUM_IRQ)
+			{
+				NVIC -> CLRPEND [Copy_u8IntReqNum/BITS_NUM] |= (1<<(Copy_u8IntReqNum % BITS_NUM));
+			}
+	else
+	{
+		Local_u8Error = status_NOk;
+	}
+	return (Local_u8Error);
 
 
 }
